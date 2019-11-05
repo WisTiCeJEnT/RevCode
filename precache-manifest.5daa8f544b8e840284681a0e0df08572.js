e649bee93554932494044b142cf272dce826a3bd
Tanakorn Prangsriarun <tanagon601@gmail.com>
2019-11-05 21:46:01 +0700

    Updates

---
+++ precache-manifest.5daa8f544b8e840284681a0e0df08572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d7127ecf44e2d6bec0aa63cc7a928a",
+    "url": "/RevCode/index.html"
+  },
+  {
+    "revision": "37c47fb622d9c9df4e44",
+    "url": "/RevCode/static/css/main.d4449b00.chunk.css"
+  },
+  {
+    "revision": "e38c149652e76c96b10c",
+    "url": "/RevCode/static/js/2.b018fe88.chunk.js"
+  },
+  {
+    "revision": "37c47fb622d9c9df4e44",
+    "url": "/RevCode/static/js/main.da1b9173.chunk.js"
+  },
+  {
+    "revision": "7c27652ebe7a661c0dd3",
+    "url": "/RevCode/static/js/runtime-main.df8512ae.js"
+  }
+]);
